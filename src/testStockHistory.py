b1ce5b632f52479157e6300571c5b65d73b70b0c
Jorge-Saura <jorsaura@gmail.com>
2020-10-31 17:31:29 +0100

    Añadidos los metodos para traer la página inicial y la página filtrada

--- src/testStockHistory.py
+++ src/testStockHistory.py
@@ -5,11 +5,13 @@ from stock import StockScraper
 from companyhistory import historicScraper
 from testfixtures import TempDirectory, compare
 
+from datetime import date, timedelta
+
 class TestHistoricStocks(unittest.TestCase):
 
     def setUp(self):
         self.url = "https://www.bolsamadrid.es"
-        self.domain = "/esp/aspx/Empresas/InfHistorica.aspx?ISIN=ES0125220311&ClvEmis=25220"
+        self.domain = "/esp/aspx/Empresas/InfHistorica.aspx?ISIN=" #ES0125220311&ClvEmis=25220"
         
         pass
 
@@ -18,11 +20,28 @@ class TestHistoricStocks(unittest.TestCase):
         pass
 
 
-    def test_get_algo(self):
-        print('hola')
+    def test_get_getHTML(self):
         
-  
+        scraper = historicScraper(self.url,self.domain)
+        isin = 'ES0125220311'
+        response = scraper._historicScraper__getPageHistoric(isin)
 
+        self.assertTrue(response.ok)
+        
+    def test_get_getHTML_filtered(self):
+            
+        scraper = historicScraper(self.url,self.domain)
+        isin = 'ES0125220311'
+        response = scraper._historicScraper__getPageHistoric(isin)
+        historicFiltered = scraper._historicScraper__getPageHistoric_Filtered(response)
+
+        self.assertTrue(historicFiltered.ok)
+
+    # def test_get_getHTML_filtered(self):
+    #     today = date.today()
+    #     initialDate = today - timedelta(days=+730)
+    #     print("Hoy")
+    #     print(initialDate.year)
  
 if __name__ == '__main__':
     # unittest.main()
